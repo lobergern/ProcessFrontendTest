e10969f193fd7678755d2499dbaaf005104361eb
Andrew Scott <atscott01@gmail.com>
2014-02-17 16:55:44 -0600

    added service for getting pages and display them in text on view 1

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,16 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var controllers = angular.module('myApp.controllers', []);
+controllers.controller('MyCtrl1', ['$scope', 'PageManager', function ($scope, PageManager) {
+  $scope.getPages = function(){
+    PageManager.getPages().then(function(response){
+      $scope.pages = response.data.pages;
+    });
+  };
 
-  }])
-  .controller('MyCtrl2', [function() {
+  $scope.getPages();
+}]);
+controllers.controller('MyCtrl2', [function () {
 
-  }]);
+}]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,36 @@
 'use strict';
 
 /* Services */
-
+var pages = [
+  {youtubeLink: 'http://www.youtube.com/watch?v=d2ZNaLQD60Y', title: 'Game Of Thrones', description: 'description', rating: 5},
+  {youtubeLink: 'http://www.youtube.com/watch?v=PRYjmBQ2W3s', title: 'Wall', description: 'description', rating: 3}
+];
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var services = angular.module('myApp.services', []);
+
+services.value('version', '0.1');
+
+services.factory('PageManager', ['$q', function ($q) {
+  return {
+    getPages: function () {
+      var deferred = $q.defer();
+      deferred.resolve({data: {pages: pages}, status: 200});
+      return deferred.promise;
+    },
+    addPage: function(page){
+      var deferred = $q.defer();
+      pages.push(page);
+      deferred.resolve({data: 'success', status: 200});
+      return q.promise;
+    },
+    removePage:function(page)
+    {
+      var deferred = $q.defer();
+      pages.splice(pages.indexOf(page),1);
+      deferred.resolve({data: 'success', status: 200});
+      return q.promise;
+    }
+  }
+}]);
