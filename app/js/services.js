a6a50125af2d93b3a6bba29d090a82f1de6f6220
atscott <atscott01@gmail.com>
2014-02-18 08:43:45 -0600

    added some user authentication methods

--- app/js/services.js
+++ app/js/services.js
@@ -6,11 +6,13 @@ var pages = [
   {youtubeLink: 'http://www.youtube.com/watch?v=PRYjmBQ2W3s', title: 'Wall', description: 'description', rating: 3}
 ];
 
+var users = [
+  {email: 'atscott01@gmail.com', password: 'Scott'}
+];
+
 // Demonstrate how to register services
 // In this case it is a simple value service.
 var services = angular.module('myApp.services', []);
-
-services.value('version', '0.1');
 
 services.factory('PageManager', ['$q', function ($q) {
   return {
@@ -19,18 +21,48 @@ services.factory('PageManager', ['$q', function ($q) {
       deferred.resolve({data: {pages: pages}, status: 200});
       return deferred.promise;
     },
-    addPage: function(page){
+    addPage: function (page) {
       var deferred = $q.defer();
       pages.push(page);
       deferred.resolve({data: 'success', status: 200});
       return deferred.promise;
     },
-    removePage:function(page)
-    {
+    removePage: function (page) {
       var deferred = $q.defer();
-      pages.splice(pages.indexOf(page),1);
+      pages.splice(pages.indexOf(page), 1);
       deferred.resolve({data: 'success', status: 200});
       return deferred.promise;
+    }
+  }
+}]);
+
+services.factory('UserAuthentication', ['$q,', function ($q) {
+  var currentUser = null;
+  return{
+    login: function (email, password) {
+      var deferred = $q.defer();
+      var userFound = false;
+      $.each(users, function () {
+        if (this.email == email) {
+          userFound = true;
+          if (this.password == password) {
+            deferred.resolve({data: 'success', status: 200})
+            currentUser = this;
+          } else {
+            deferred.resolve({data: 'incorrect password', status: 400});
+          }
+        }
+      });
+      if (!userFound) {
+        users.push({email: email, password: password});
+        currentUser = users[users.size - 1];
+        deferred.resolve({data: 'success', status: 200});
+      }
+      return deferred.promise;
+    },
+    getCurrentUser: currentUser,
+    logout: function () {
+      currentUser = null;
     }
   }
 }]);
