0a11a8f6bda8a6e8143ac31151248a607002bbc1
unknown <atscott01@gmail.com>
2014-02-23 02:05:42 +0000

    conditional details page css based on video being defined. also added tests around login/logout service

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -18,7 +18,8 @@ describe('service', function () {
       }));
 
       it('should notify page observers when add page is successfully called', function () {
-        var callbackTest = {callback: function(){}};
+        var callbackTest = {callback: function () {
+        }};
         spyOn(callbackTest, 'callback');
         service.registerPageObserverCallback(callbackTest.callback);
         $httpBackend.when('POST', serverBaseUrl + '/content').respond(200, {status: 200, data: {}});
@@ -28,7 +29,8 @@ describe('service', function () {
       });
 
       it('should NOT notify page observers when add page is called and returns error', function () {
-        var callbackTest = {callback: function(){}};
+        var callbackTest = {callback: function () {
+        }};
         spyOn(callbackTest, 'callback');
         service.registerPageObserverCallback(callbackTest.callback);
         $httpBackend.when('POST', serverBaseUrl + '/content').respond(500);
@@ -36,6 +38,47 @@ describe('service', function () {
         $httpBackend.flush();
         expect(callbackTest.callback).not.toHaveBeenCalled();
       });
+    });
+
+
+    describe("Authentication Service:", function () {
+      var service, $httpBackend;
+      beforeEach(inject(function (Authentication, _$httpBackend_) {
+        service = Authentication;
+        $httpBackend = _$httpBackend_;
+      }));
+
+      it('should notify observers when a user logs out', function () {
+        var callbackTest = {callback: function (user) {
+        }};
+        spyOn(callbackTest, 'callback');
+        service.registerUserChangeCallback(callbackTest.callback);
+        service.logout();
+        expect(callbackTest.callback).toHaveBeenCalled();
+      });
+
+      it('should notify observers when a user logs in', function () {
+        var callbackTest = {callback: function (user) {
+        }};
+        spyOn(callbackTest, 'callback');
+        service.registerUserChangeCallback(callbackTest.callback);
+        service.login('email', 'password');
+        $httpBackend.when('POST', serverBaseUrl +'/user').respond(200);
+        $httpBackend.flush();
+        expect(callbackTest.callback).toHaveBeenCalled();
+      });
+
+      it('should NOT notify observers when a log in fails', function(){
+          var callbackTest = {callback: function (user) {
+        }};
+        spyOn(callbackTest, 'callback');
+        service.registerUserChangeCallback(callbackTest.callback);
+        service.login('email', 'password');
+        $httpBackend.when('POST', serverBaseUrl +'/user').respond(500);
+        $httpBackend.flush();
+        expect(callbackTest.callback).not.toHaveBeenCalled();
+      });
+
     });
   });
 
